c0e09b561fc7e1d219cb3cfa6d1add6967452a84
quangtep9x <131895097+quangtep9x@users.noreply.github.com>
2023-04-26 23:32:30 +0700

    Create getcode.py

---
+++ getcode.py
@@ -0,0 +1,46 @@
+import json , time, random,string , requests
+
+getcode(token)
+
+def getcode(token):
+      while True:
+              auth_headers = {
+                      "accept": "application/ld+json",
+                      "Content-Type": "application/json",
+                      "Authorization": "Bearer {}".format(token)
+              }
+
+              r = requests.get("https://api.mail.tm/messages?page=1",headers=auth_headers)
+
+
+              getms = r.json()
+              if getms['hydra:member'] == []:
+                      print('Chưa về mã')
+                      time.sleep(2)
+                      # print(getms)
+              else:
+                      print('Đã có mã!')
+                      break
+
+      message = []
+
+      for message_data in getms['hydra:member']:
+              auth_headers = {
+              "accept": "application/ld+json",
+              "Content-Type": "application/json",
+              "Authorization": "Bearer {}".format(token)
+              }
+
+              r = requests.get(f"https://api.mail.tm/messages/{message_data['id']}", headers=auth_headers)
+              full_message_json = r.json()
+              text = full_message_json["text"]
+              html = full_message_json["html"]
+              #print('html', html)
+              # print(html[0])
+              try:
+                      code = html[0].split('solid #ccc;">FB-')[1].split('</td>')[0]
+                      print('code: ', code)
+                      return code
+                      break
+              except:
+                      pass
